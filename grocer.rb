881e7e6fae113567179087ec44e78394ff2d4f12
Kyle Bradley <kylepersonalmail@gmail.com>
2020-03-25 15:07:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -76,8 +76,8 @@ def apply_clearance(cart)
   after_clearance_array = []
   element_index = 0 
   while element_index < cart.legnth do 
+    binding.pry
     if cart[element_index][:clearance]
-      binding.pry
       og_price = cart[element_index][:price]
       cart[element_index][:price] = (og_price * 0.8).round(2)
       
