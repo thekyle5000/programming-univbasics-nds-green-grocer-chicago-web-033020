69f765c3c1b01ce6b514d4bf9b82550921848602
Kyle Bradley <kylepersonalmail@gmail.com>
2020-03-25 14:35:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,29 +1,86 @@
+require 'pry'
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
+ element_index = 0
+ result = nil
+ while element_index < collection.length do 
+   if collection[element_index][:item] == name
+     result = collection[element_index]
+   end 
+   element_index += 1 
+ end 
+ result
+ end
+
+
+ 
+      # Implement me first!
   #
   # Consult README for inputs and outputs
-end
+
 
 def consolidate_cart(cart)
+  cart_array = []
+  element_index  = 0
+  while element_index < cart.length do 
+    x = find_item_by_name_in_collection(cart[element_index][:item], cart_array)
+    if x
+      x[:count] += 1
+    else 
+      cart[element_index][:count] = 1
+      cart_array << cart[element_index]
+    end 
+    element_index += 1
+  end 
+  cart_array 
+end
+
+  
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-end
 
 def apply_coupons(cart, coupons)
+  element_index = 0
+  after_coupons_array = []
+  while element_index < cart.length do
+  x = find_item_by_name_in_collection(cart[element_index][:item], coupons)
+  if x && cart[element_index][:count] >= x[:num]
+    coupon_hash = {} 
+    coupon_hash[:clearance] = cart[element_index][:clearance]
+    coupon_hash[:item] = x[:item] += " W/COUPON"
+    coupon_hash[:price] = x[:cost] / x[:num]
+    coupon_hash[:count] = x[:num]
+    after_coupons_array << coupon_hash
+    cart[element_index][:count] -= x[:num]
+    after_coupons_array << cart[element_index]
+  elsif coupons == []
+    after_coupons_array = cart
+  else 
+    after_coupons_array << cart[element_index]
+  end
+  element_index += 1
+  end
+after_coupons_array
+end
+   # cart[element_index][:count] -= x[:num]
+    # after_coupons_array << cart[element_index]
+
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-end
 
-def apply_clearance(cart)
+
+ def apply_clearance(cart)
+   
+ end
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-end
 
-def checkout(cart, coupons)
+
+# def checkout(cart, coupons)
+# end
   # Consult README for inputs and outputs
   #
   # This method should call
@@ -33,4 +90,3 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-end
